8a90b599f8716c063cf6604f2f9b614f6584a2a8
davi-mbatista <machadobatista.davi@gmail.com>
2020-08-05 08:33:13 +0200

    Update

--- src/index.svelte
+++ src/index.svelte
@@ -1,9 +1,13 @@
 <div class="p-5 h-screen flex justify-center items-center">
 
-    <section class="text-2xl">
-        <span class="p-2 bg-red-600 text-white rounded-lg">Svelte</span>
-        <span class="text-gray-600">+</span>
-        <span class="p-2 bg-teal-600 text-white rounded-lg">Tailwind CSS</span>
+    <section class="text-2xl text-gray-600">
+        <span class="p-2 bg-red-600 text-white rounded-lg shadow-sm">
+            Svelte
+        </span>
+        {' '}+
+        <span class="p-2 bg-teal-600 text-white rounded-lg shadow-sm">
+            Tailwind CSS
+        </span>
     </section>
 
     <footer class="flex absolute bottom-0 mb-5 text-gray-700">
@@ -11,7 +15,7 @@
         <a
             class="ml-1 text-gray-800 underline"
             href="https://github.com/davi-mbatista/start"
-            alt="Github's start template project"
+            alt="GitHub's start template project"
             target="_blank"
             rel="noopener noreferrer">
             /start
