400d51539de41d6f56b92222f13872d3ffe04ec4
davi-mbatista <machadobatista.davi@gmail.com>
2021-02-16 22:40:18 +0100

    Update

--- webpack/utils/plugins.js
+++ webpack/utils/plugins.js
@@ -3,12 +3,9 @@ const paths = require('./paths');
 
 const plugins = {
     Clean: require('clean-webpack-plugin').CleanWebpackPlugin,
-    Copy: require('copy-webpack-plugin'),
     Define: require('webpack').DefinePlugin,
     Html: require('html-webpack-plugin'),
     MiniCSSExtract: require('mini-css-extract-plugin'),
-    OptimizeCSSAssets: require('optimize-css-assets-webpack-plugin'),
-    ScriptExtensionHTML: require('script-ext-html-webpack-plugin'),
     Size: require('size-plugin')
 };
 
@@ -29,11 +26,6 @@ module.exports = {
             filename: 'static/css/[name].[chunkhash:8].css',
             chunkFilename: 'static/css/[id].[chunkhash:8].css'
         }),
-        new plugins.OptimizeCSSAssets({
-            cssProcessorPluginOptions: {
-                preset: ['default', { discardComments: { removeAll: true } }]
-            }
-        }),
         new plugins.Html({
             inject: 'body',
             template: paths.APP_HTML,
@@ -44,21 +36,6 @@ module.exports = {
                 removeRedundantAttributes: true,
                 removeComments: true
             }
-        }),
-        new plugins.ScriptExtensionHTML({
-            defaultAttribute: 'async'
-        }),
-        new plugins.Copy({
-            patterns: [
-                {
-                    context: paths.APP_PUBLIC,
-                    from: './**/*',
-                    to: './',
-                    globOptions: {
-                        ignore: ['*.html']
-                    }
-                }
-            ]
         }),
         new plugins.Size()
     ]
